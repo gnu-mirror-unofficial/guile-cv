6bf5b7d0576f84bea63dcf8aba4cb497e75d3170
David Pirotte <david@altosw.be>
2017-01-21 19:27:49 -0200

    Initializing git - step 2
    * Makefile.am:
    * am/guile.mk:
    * autogen.sh:
    * configure.ac:
    * m4/guile.m4:
    * meta/guile-cv-1.0.pc.in:
    * pre-inst-env.in:  New files.

---
+++ am/guile.mk
@@ -0,0 +1,42 @@
+
+####
+#### Copyright (C) 2016
+#### Free Software Foundation, Inc.
+
+#### This file is part of GNU Guile-CV
+
+#### GNU Guile-CV is free software; you can redistribute it and/or
+#### modify it under the terms of the GNU Lesser General Public
+#### License as published by the Free Software Foundation; either
+#### version 3 of the License, or (at your option) any later version.
+
+#### GNU Guile-CV is distributed in the hope that it will be
+#### useful, but WITHOUT ANY WARRANTY; without even the implied
+#### warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#### See the GNU Lesser General Public License for more details.
+
+#### You should have received a copy of the GNU Lesser General Public
+#### License along with GNU Guile-CV.  If not, see
+#### <https://www.gnu.org/licenses/lgpl.html>.
+####
+
+
+GOBJECTS = $(SOURCES:%.scm=%.go)
+
+nobase_mod_DATA = $(SOURCES) $(NOCOMP_SOURCES)
+nobase_go_DATA = $(GOBJECTS)
+
+# Make sure source files are installed first, so that the mtime of
+# installed compiled files is greater than that of installed source
+# files.  See
+# <http://lists.gnu.org/archive/html/guile-devel/2010-07/msg00125.html>
+# for details.
+guile_install_go_files = install-nobase_goDATA
+$(guile_install_go_files): install-nobase_modDATA
+
+GUILE_WARNINGS = -Wunbound-variable -Warity-mismatch -Wformat
+
+SUFFIXES = .scm .go
+.scm.go:
+	$(AM_V_GEN)$(top_builddir)/pre-inst-env \
+	guild compile $(GUILE_WARNINGS) -o "$@" "$<"
