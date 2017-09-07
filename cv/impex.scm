gnu-mirror-unofficial/guile-cv

commit d0bd28308f01801e042689a26e65c8b16fd779c9

Interface change - im-save, adding (scale #f) optional argument
* cv/impex.scm (im-save): Adding an optional (scale #f) argument.  Till
  now, images pixel values would be always scaled at save time, unless
  saving the file using tif.  This was really less then optimal
  because (a) tif preserved values, but no image viewer knows how to
  display these Guile-CV 32 bit float images (they beleive these are
  RGBA images) and (b) scaling at save time totally destroys the result
  of im memory calcuations ... What we really want here, most of the
  time at least, and hence it becomes the default from now on, is what
  is called 'clipping': pixel values are ‘clipped’, which means that
  values < 0 are saved as 0, values > 255 are saved as 255, and
  otherwise are saved unchanged.  Bingo, now WYSIWYC (what you see is
  what you computed :-) ...

  Note that this required a patch on Vigra C itself, so from this
  Guile-CV commit you will require an updated Vigra C, a version that
  includes the following commit (or later of course):

	c2f35f7bd85764cdd4e9acd4050d3d6ea16acf89

* doc/api-cv-impex.texi:  Updated for the above.

cv/impex.scm
@@ -72,16 +72,16 @@
        (else
         (error "Not a GRAY, RGB nor an RGBA image" filename))))))
 
-(define (im-save image filename)
+(define* (im-save image filename #:optional (scale #f))
   (match image
     ((width height n-chan idata)
      (case n-chan
        ((1)
-        (vigra-save-gray-image image filename width height))
+        (vigra-save-gray-image image filename width height scale))
        ((3)
-        (vigra-save-rgb-image image filename width height))
+        (vigra-save-rgb-image image filename width height scale))
        ((4)
-        (vigra-save-rgba-image image filename width height))
+        (vigra-save-rgba-image image filename width height scale))
        (else
         (error "Not a GRAY, RGB nor an RGBA image" filename))))))
 
@@ -154,18 +154,21 @@
          ((0) (list width height 4 idata))
          ((1) (error "Load RGBA image failed." filename)))))))
 
-(define (vigra-save-gray-image image filename width height)
+(define* (vigra-save-gray-image image filename width height
+                                #:optional (scale #f))
   (match (im-channels image)
     ((c)
      (case (vigra-exportgrayimage-c (bytevector->pointer c)
                                     width
                                     height
-                                    (string->pointer filename))
+                                    (string->pointer filename)
+                                    (if scale 1 0))
        ((0) #t)
        (else
         (error "Image could not be saved." filename))))))
 
-(define (vigra-save-rgb-image image filename width height)
+(define* (vigra-save-rgb-image image filename width height
+                               #:optional (scale #f))
   (match (im-channels image)
     ((r g b)
      (case (vigra-exportrgbimage-c (bytevector->pointer r)
@@ -173,12 +176,14 @@
                                    (bytevector->pointer b)
                                    width
                                    height
-                                   (string->pointer filename))
+                                   (string->pointer filename)
+                                   (if scale 1 0))
        ((0) #t)
        (else
         (error "Image could not be saved." filename))))))
 
-(define (vigra-save-rgba-image image filename width height)
+(define* (vigra-save-rgba-image image filename width height
+                                #:optional (scale #f))
   (match (im-channels image)
     ((r g b a)
      (case (vigra-exportrgbaimage-c (bytevector->pointer r)
@@ -187,7 +192,8 @@
                                     (bytevector->pointer a)
                                     width
                                     height
-                                    (string->pointer filename))
+                                    (string->pointer filename)
+                                    (if scale 1 0))
        ((0) #t)
        (else
         (error "Image could not be saved." filename))))))
@@ -237,7 +243,7 @@
   (pointer->procedure int
 		      (dynamic-func "vigra_exportgrayimage_c"
 				    %libvigra-c)
-		      (list '* int int '*)))
+		      (list '* int int '* int)))
 
 (define vigra-exportrgbimage-c
   (pointer->procedure int

doc/api-cv-impex.texi
@@ -42,18 +42,36 @@ JPEG, BMP, EXR, HDR, PNM (PBM, PGM, PPM), PNG, SunRaster, KHOROS-VIFF.
 
 
 @anchor{im-save}
-@deffn Procedure im-save image filename
+@deffn Procedure im-save image filename [scale #f]
 
 Returns #t.
 
-Saves @var{image} in @var{filename}.  The type in which @var{image} is
-saved is determined by the @var{filename} extension.
+Saves @var{image} in @var{filename}.
+
+The optional @var{scale} argument can take the following values:
+
+@indentedblock
+@table @code
+@item #f
+pixel values are @samp{clipped}: values @code{< 0} are saved as
+@code{0}, values @code{> 255} are saved as @code{255}, and otherwise are
+saved unchanged
+
+@item #t
+all pixel values are scaled@footnote{Note that in this particular
+context, @code{scale} does not mean a change in dimension, but rather
+bringing pixel values from the range they occupy in memory to the
+@code{[0 255]} range} to the @code{[0 255]} range
+@end table
+@end indentedblock
+
+The type in which @var{image} is saved is determined by the
+@var{filename} extension, as in the folowing example:
 
 @lisp
-,use (cv)
-(define img-1 (im-load "images/lenna.png"))
+(im-load "edx.png")
 ...
-(im-save img-1 "/tmp/lenna.jpg")
+(im-save $4 "/tmp/edx.jpg")
 @end lisp
 
 @end deffn

commit 143a03275c282d8eeb9ed3de3d717c15874183fc

Fixing a bug introduced by the recent commit d0bd283
* cv/impex.scm (vigra-exportrgbimage-c, vigra-exportrgbaimage-c): Adding
  int as the last of the list of arguments for both
  vigra-exportrgbimage-c and vigra-exportrgbaimage-c, just like we did
  for vigra-exportgrayimage-c in commit d0bd283 actually, which is a
  boolean passed to Vigra C, the default being 0, that determines if
  pixel values will be scaled or clipped as the RGB or RGBA image is
  saved.

cv/impex.scm
@@ -249,10 +249,10 @@
   (pointer->procedure int
 		      (dynamic-func "vigra_exportrgbimage_c"
 				    %libvigra-c)
-		      (list '* '* '* int int '*)))
+		      (list '* '* '* int int '* int)))
 
 (define vigra-exportrgbaimage-c
   (pointer->procedure int
 		      (dynamic-func "vigra_exportrgbaimage_c"
 				    %libvigra-c)
-		      (list '* '* '* '* int int '*)))
+		      (list '* '* '* '* int int '* int)))