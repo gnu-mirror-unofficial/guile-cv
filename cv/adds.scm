16ab85e2ac1affce064f450c802e569f2260cac7
David Pirotte <david@altosw.be>
2017-11-21 23:37:56 -0200

    Performance improvements - im-add, im-add-channel
    * cv/adds.scm: Updated to use f32vector-add-vectors.  Adding two RGB
      images of 2718 x 1732 now takes 0.06 sec instead of 2.5 sec, quite an
      improvement. Note that all memory allocation is still kept in scheme,
      so we are on the safe side. Finally, this operation is multithreaded
      on multi channel images, so the measure here represents 3 threads
      performing each +- 5millions floating point additions. In image
      processing 'real world', this would be a tiny image, being 46x faster
      for these linear algebra basic ops will make a huge diff.  Till we
      have Guile-3.0 and since all mem allocation is in scheme still, I am
      confortable with this approach.

--- cv/adds.scm
+++ cv/adds.scm
@@ -328,12 +328,35 @@ Target.B = ((1 - Source.A) * BGColor.B) + (Source.A * Source.B)
 (define-method (im-add-channel channel width height (val <number>))
   (im-map-channel (lambda (p-val) (+ p-val val)) width height channel))
 
-(define-method (im-add . images)
+#;(define-method (im-add . images)
   (apply im-map + images))
 
-(define-method (im-add-channel width height . channels)
+#;(define-method (im-add-channel width height . channels)
   (apply im-map-channel + width height channels))
 
+(define-method (im-add . images)
+  (match images
+    ((image . rest)
+     (match image
+       ((width height n-chan _)
+	(if (and (apply = (apply im-collect 'width rest))
+		  (apply = (apply im-collect 'height rest))
+		  (apply = (apply im-collect 'n-channel rest)))
+            (list width height n-chan
+                  (let ((map-proc (if (and (> n-chan 1)
+                                           (%use-par-map)) par-map map)))
+                    (map-proc (lambda (channels)
+                                (apply im-add-channel width height channels))
+                        (apply zip (apply im-collect 'channels images)))))
+	    (error "Size mismatch.")))))
+    ((image) image)
+    (()
+     (error "Invalid argument: " images))))
+
+(define-method (im-add-channel width height . channels)
+  (let ((n-cell (* width height))
+        (to (im-make-channel width height)))
+    (f32vector-add-vectors to n-cell channels)))
 
 (define-method (im-subtract image (val <number>))
   (im-map (lambda (p-val) (- p-val val)) image))
