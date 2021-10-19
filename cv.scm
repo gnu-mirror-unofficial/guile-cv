90990a8efe5db44ca250a8e2c1e4815530ed1295
David Pirotte <david@altosw.be>
2021-10-19 02:43:24 -0300

    Bumping (C) years (unintentionally 'skipped' in the previous commit)
    * cv.scm:
    * cv/support/goops.scm: Bumping (C) years (unintentionally 'skipped' in
      the previous commit).

--- cv.scm
+++ cv.scm
@@ -1,7 +1,7 @@
 ;; -*- mode: scheme; coding: utf-8 -*-
 
 ;;;;
-;;;; Copyright (C) 2016 - 2017
+;;;; Copyright (C) 2016 - 2021
 ;;;; Free Software Foundation, Inc.
 
 ;;;; This file is part of GNU Guile-CV.
